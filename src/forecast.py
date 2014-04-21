692af9d2f50b45f6e70feb98ca749d39e839de6a
Christian Treppo <christian@treppo.org>
2014-04-21 16:31:11 +0100

    Show if it is going to rain

---
+++ src/forecast.py
@@ -0,0 +1,18 @@
+import os
+
+from datetime import datetime
+
+import forecastio
+
+def is_precip_type_rain(data_point):
+    return data_point.precipType == 'rain'
+
+def is_raining_in_any(data_points):
+    len(filter(is_precip_type_rain, data_points)) > 0
+
+def is_raining(coordinates):
+    api_key = os.environ['FORECASTIO_KEY']
+    lat, lng = coordinates.split(',')
+    forecast = forecastio.load_forecast(api_key, lat, lng, datetime.today())
+
+    return 'Yup!' if is_raining_in_any(forecast.hourly().data) else 'Nope!'
--- src/main.py
+++ src/main.py
@@ -4,18 +4,25 @@ import os
 import webapp2
 import jinja2
 
+from forecast import is_raining
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     autoescape=True,
     extensions=['jinja2.ext.autoescape'])
 
+DEFAULT_CITY = 'Dublin'
+DEFAULT_COORDINATES = '53.3494299,-6.2600969'
+
 class MainHandler(webapp2.RequestHandler):
 
   def get(self):
     template = JINJA_ENVIRONMENT.get_template('main.html')
+    city = self.request.headers.get('X-AppEngine-City') or DEFAULT_CITY
+    coordinates = self.request.headers.get('X-AppEngine-CityLatLong') or DEFAULT_COORDINATES
     variables = {
-            'location': 'Dublin',
-            'is_raining': 'YUP!',
+            'location': city,
+            'is_raining': is_raining(coordinates),
             }
 
     self.response.write(template.render(variables))
