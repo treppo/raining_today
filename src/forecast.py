831e7ced9d37a456427f2f132cf71c56e236d4f8
Christian Treppo <christian@treppo.org>
2014-04-24 11:08:31 +0100

    Fix bug with rain check and remove past from weather data

--- src/forecast.py
+++ src/forecast.py
@@ -7,12 +7,15 @@ def is_precip_type_rain(data_point):
     return data_point.precipType == 'rain'
 
 def is_raining_in_any(data_points):
-    len(filter(is_precip_type_rain, data_points)) > 0
+    return len(filter(is_precip_type_rain, data_points)) > 0
+
+def remove_past(data_points):
+    return data_points[datetime.now().hour - 1:]
 
 def hourly_weather_for(location):
     api_key = os.environ['FORECASTIO_KEY']
     forecast = forecastio.load_forecast(api_key, location.lat, location.lon, datetime.today())
-    return forecast.hourly().data
+    return remove_past(forecast.hourly().data)
 
 def is_raining(location):
     return is_raining_in_any(hourly_weather_for(location))
